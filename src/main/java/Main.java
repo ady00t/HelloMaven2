1091e376e59162c922b44901b0154e9730100bcd
Adi Tanase <ady00t@live.com>
2015-05-14 18:21:23 +0200

    adding the rest of the tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+testMethodOne(10,30);
 	}
 
-}
+	public double testMethodOne(int a, int b) {
+        return a/b;
+    }
+	}
+
