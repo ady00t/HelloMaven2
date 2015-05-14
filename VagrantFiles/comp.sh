f92250e03900b3c54a3c6cc27c47c0b8a648c913
Adi Tanase <ady00t@live.com>
2015-05-14 21:17:27 +0200

    adding the vagrant file

---
+++ VagrantFiles/comp.sh
@@ -0,0 +1,14 @@
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+#intsalling git
+sudo apt-get update
+sudo apt-get -y install git
+
+# this will install jenkins
+
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y jenkins
+sudo apt-get update
